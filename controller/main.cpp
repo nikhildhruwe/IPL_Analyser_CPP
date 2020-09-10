eec4fce517e1b5fe98f30c7127ea32915be8d485
nikhildhruwe <nick.dhruwe@gmail.com>
2020-09-10 22:39:58 +0530

    Added: Code to get top strike rate from ipl data file

--- controller/main.cpp
+++ controller/main.cpp
@@ -3,13 +3,40 @@
 #include "../view/IPLAnalyserDisplay.h"
 using namespace std;
 
-int main(){
-    const string MOST_RUNS_CSV_FILE_PATH = "../resources/IPL2019FactsheetMostRuns.csv";
-    IPLAnalyserDisplay displayObj;
-    displayObj.displayWelcomeMessage();
+const string MOST_RUNS_CSV_FILE_PATH = "../resources/IPL2019FactsheetMostRuns.csv";
 
-    IPLAnalyser analyser;
-    vector<IPLMostRuns> batsmanDetailsList = analyser.loadCSVData(MOST_RUNS_CSV_FILE_PATH);
-    IPLMostRuns batsman =  analyser.getBatsmanWithHighestAverage(batsmanDetailsList);
-    displayObj.displayBatsmanWithHighestAverage(batsman);
+class IPLController{
+    private :
+        IPLAnalyserDisplay iplDisplayObj;
+        IPLAnalyser iplAnalyserObj;
+        vector<IPLMostRuns> batsmanDetailsList;
+    public :
+        IPLController(){
+            batsmanDetailsList = iplAnalyserObj.loadCSVData(MOST_RUNS_CSV_FILE_PATH);
+        }
+
+        void displayWelcomeMessage(){
+            iplDisplayObj.displayWelcomeMessage();
+        }
+
+        void displayBatsmanWithHighestAverage(){
+            vector<IPLMostRuns> batsmanList =  iplAnalyserObj.getBatsmanWithHighestAverage(batsmanDetailsList);
+            string message = "Top BAtting Average : ";
+            iplDisplayObj.displayBatsmanDetails(batsmanList, message);
+        }
+
+        void displayBatsmanWithTopStrikingRates(){
+            vector<IPLMostRuns> batsmanList =  iplAnalyserObj.getBatsmanWithTopStrikeRate(batsmanDetailsList);
+            string message = "Top Strike Rate :";
+            iplDisplayObj.displayBatsmanDetails(batsmanList, message);
+        }
+};
+
+int main(){
+   IPLController controller;
+   controller.displayWelcomeMessage();
+   controller.displayBatsmanWithHighestAverage();
+   controller.displayBatsmanWithTopStrikingRates();
+
+   return 0;
 }
